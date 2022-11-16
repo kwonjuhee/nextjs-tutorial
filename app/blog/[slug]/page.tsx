349682930a4901d413f0a0e793259ef4a4526129
juhee <77ooee@gmail.com>
2022-11-16 16:08:40 +0900

    use client component

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Counter from "./counter";
+
 export async function generateStaticParams() {
   return [
     {
@@ -10,6 +12,7 @@ export default function IndividualBlogPage() {
   return (
     <div>
       <h1>hi this is post 1</h1>
+      <Counter />
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ea sed
       sequi doloribus saepe alias? A quo quaerat harum nam exercitationem
       corporis consequuntur excepturi nesciunt possimus? Quisquam eos assumenda
